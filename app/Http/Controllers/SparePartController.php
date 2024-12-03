25d5cd98f0f9c48e8078ea752bb37933326c0ea9
Roman-Ast <roman_planeta@mail.ru>
2024-12-03 14:20:25 +0500

    try phaeton35

--- app/Http/Controllers/SparePartController.php
+++ app/Http/Controllers/SparePartController.php
@@ -176,7 +176,7 @@ class SparePartController extends Controller
             'ApiKey' => 'LnxrDfpQVZz1ncuoI14e',
             'Article' => $partnumber,
             'Brand' => $brand,
-            "Sources[]" => 1,
+            "Sources[]" => 3,
             'includeAnalogs' => true,
         ];
 
