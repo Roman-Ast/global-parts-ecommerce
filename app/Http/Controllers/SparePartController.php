567ae6b918ac1f2bfb9064341884e909f5156b0f
Roman-Ast <roman_planeta@mail.ru>
2024-12-03 14:26:12 +0500

    try phaeton38

--- app/Http/Controllers/SparePartController.php
+++ app/Http/Controllers/SparePartController.php
@@ -176,8 +176,7 @@ class SparePartController extends Controller
             'ApiKey' => 'LnxrDfpQVZz1ncuoI14e',
             'Article' => $partnumber,
             'Brand' => $brand,
-            "Sources[]" => 1,
-            "Sources[]" => 2,
+            "Sources[]" => 3,
         ];
 
         curl_setopt($ch, CURLOPT_URL, $resUrl . http_build_query($params));
