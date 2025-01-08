0101d4a75cf17ec7e20574493f0cd9f0dd6f7663
Roman-Ast <roman_planeta@mail.ru>
2025-01-08 23:08:29 +0500

    supplier color try3

--- resources/views/partSearchRes.blade.php
+++ resources/views/partSearchRes.blade.php
@@ -62,7 +62,7 @@
                 @if (count($finalArr['searchedNumber']) > 0)
                     @foreach ($finalArr['searchedNumber'] as $searchItem)
                         <div class="requestPartNumberContainer-item">
-                            <div class="requestPartNumberContainer-item-entity requestPartNumber-supplier" style="text-shadow:{{ $searchItem['supplier_color'] }} 1px 0 10px">
+                            <div class="requestPartNumberContainer-item-entity requestPartNumber-supplier" style="text-shadow:{{ $crossItem['supplier_color'] }} 1px 2px 2px">
                                 {{ $searchItem['supplier_city'] }}
                             </div>
                             <div class="requestPartNumberContainer-item-entity requestPartNumber-brand">
@@ -118,7 +118,7 @@
             <div id="crossesContainer-on-stock">
                 @foreach ($finalArr['crosses_on_stock'] as $index => $crossItem)
                 <div class="requestPartNumberContainer-item">
-                    <div class="requestPartNumberContainer-item-entity requestPartNumber-supplier" style="text-shadow:{{ $crossItem['supplier_color'] }} 1px 0 10px">
+                    <div class="requestPartNumberContainer-item-entity requestPartNumber-supplier" style="text-shadow:{{ $crossItem['supplier_color'] }} 1px 2px 2px">
                         {{ $crossItem['supplier_city'] }}
                     </div>
                     <div class="requestPartNumberContainer-item-entity requestPartNumber-brand">
