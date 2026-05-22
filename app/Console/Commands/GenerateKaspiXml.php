bf7ca5310d5bbfa2b2a956feae5e8ed8541c4d31
roman <roman_planeta@mail.ru>
2026-05-22 16:30:25 +0500

    fixed kaspi feed5

--- app/Console/Commands/GenerateKaspiXml.php
+++ app/Console/Commands/GenerateKaspiXml.php
@@ -36,41 +36,40 @@ class GenerateKaspiXml extends Command
 
         $totalCount = 0;
 
-        DB::table('kaspi_initial_products')
-            ->where('price', '>', 0)
-            ->where('stock', '>', 0)
-            ->orderBy('id')
-            ->chunkById(1000, function ($products) use ($xml, &$totalCount) {
-                foreach ($products as $product) {
+       DB::table('kaspi_initial_products')
+        ->where('price', '>', 0)
+        ->where('stock', '>', 0)
+        ->orderBy('id')
+        ->chunkById(1000, function ($products) use ($xml, &$totalCount) {
+            foreach ($products as $product) {
 
-                    $cleanTitle = str_replace(['¶', '"', "'"], '', $product->title);
+                $cleanTitle = str_replace(['¶', '"', "'"], '', $product->title);
 
-                    $xml->startElement('offer');
-                    $xml->writeAttribute('sku', $product->sku);
+                $xml->startElement('offer');
+                $xml->writeAttribute('sku', $product->sku);
 
-                    $xml->writeElement('model', $cleanTitle);
-                    $xml->writeElement('brand', Str::upper($product->brand));
-                    $xml->writeElement('price', number_format($product->price, 0, '.', ''));
+                $xml->writeElement('model', $cleanTitle);
+                $xml->writeElement('brand', Str::upper($product->brand));
 
-                    $xml->startElement('availabilities');
-
-                    $xml->startElement('availability');
-                    $xml->writeAttribute('available', 'yes');
-                    $xml->writeAttribute('storeId', 'PP1');
-
-                    if (isset($product->preorder_days) && $product->preorder_days > 0) {
-                        $xml->writeAttribute('preOrder', $product->preorder_days);
-                    }
-
-                    $xml->endElement(); // availability
-                    $xml->endElement(); // availabilities
-                    $xml->endElement(); // offer
-
-                    $totalCount++;
+                $xml->startElement('availabilities');
+                $xml->startElement('availability');
+                $xml->writeAttribute('available', 'yes');
+                $xml->writeAttribute('storeId', 'PP1');
+                if (isset($product->preorder_days) && $product->preorder_days > 0) {
+                    $xml->writeAttribute('preOrder', $product->preorder_days);
                 }
+                $xml->endElement(); // availability
+                $xml->endElement(); // availabilities
 
-                $this->line("Упаковано товаров в XML: {$totalCount}...");
-            });
+                $xml->writeElement('price', number_format($product->price, 0, '.', ''));
+
+                $xml->endElement(); // offer
+
+                $totalCount++;
+            }
+
+            $this->line("Упаковано товаров в XML: {$totalCount}...");
+        });
 
         $xml->endElement(); // offers
         $xml->endElement(); // kaspi_catalog
